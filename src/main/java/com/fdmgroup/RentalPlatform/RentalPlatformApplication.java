0bd84c0c581f2646ffe16a83235a36fbffba51d6
jakub kowalczyk <jakub.kowalczyk@fdmgroup.com>
2023-01-24 16:04:51 +0100

    "Initial commit - created packages, created classes User, SecurityConfig, dependencies added"

--- src/main/java/com/fdmgroup/RentalPlatform/RentalPlatformApplication.java
+++ src/main/java/com/fdmgroup/RentalPlatform/RentalPlatformApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RentalPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RentalPlatformApplication.class, args);
+		SpringApplication.run(RentalPlatformApplication.class, args); 
 	}
 
 }
---
+++ src/main/java/com/fdmgroup/RentalPlatform/model/User.java
@@ -0,0 +1,8 @@
+package com.fdmgroup.RentalPlatform.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+}
