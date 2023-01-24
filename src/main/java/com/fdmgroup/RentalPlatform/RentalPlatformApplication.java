b216f03924d511faf0f914b6faf65aca9a5a906b
jakub kowalczyk <jakub.kowalczyk@fdmgroup.com>
2023-01-24 15:24:37 +0100

    Initial commit

---
+++ src/main/java/com/fdmgroup/RentalPlatform/RentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.RentalPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/RentalPlatform/RentalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.RentalPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
