1219dcc3f1e103d0ae2894ecf81276d130e121a9
jakub kowalczyk <jakub.kowalczyk@fdmgroup.com>
2023-01-26 14:07:57 +0100

    Merge branch 'weblayout' of https://git.fdmgroup.com/f-22-06/rentalplatform.git into main_java_skeleton

--- src/main/java/com/fdmgroup/RentalPlatform/model/Review.java
+++ src/main/java/com/fdmgroup/RentalPlatform/model/Review.java
@@ -1,5 +1,12 @@
 package com.fdmgroup.RentalPlatform.model;
 
-public class Review {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Review {
+	@Id
+	@GeneratedValue
+	private Integer id;
 }
--- src/main/java/com/fdmgroup/RentalPlatform/services/ReviewService.java
+++ src/main/java/com/fdmgroup/RentalPlatform/services/ReviewService.java
@@ -1,11 +1,12 @@
 package com.fdmgroup.RentalPlatform.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fdmgroup.RentalPlatform.repository.ReviewRepository;
 
 
-
+@Service
 public class ReviewService implements IReviewService {
 	
 	
