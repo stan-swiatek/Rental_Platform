0bd1918bc478dddee6d80900179b5d2842d765e0
Stanisław Świątek <swiatek.s93@gmail.com>
2023-01-26 20:42:48 +0100

    added in comments booking and message

--- src/main/java/com/fdmgroup/RentalPlatform/model/Booking.java
+++ src/main/java/com/fdmgroup/RentalPlatform/model/Booking.java
@@ -1,5 +1,7 @@
 package com.fdmgroup.RentalPlatform.model;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,5 +11,92 @@ public class Booking {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+//	private User owner, requester;
+//	private Product product;
+//	private String offerStatus, rolename;
+//	private Date startDate, endDate;
+//	
+//	
+//	public Booking() {
+//		
+//	}
+//
+//
+//	public User getOwner() {
+//		return owner;
+//	}
+//
+//
+//	public void setOwner(User owner) {
+//		this.owner = owner;
+//	}
+//
+//
+//	public User getRequester() {
+//		return requester;
+//	}
+//
+//
+//	public void setRequester(User requester) {
+//		this.requester = requester;
+//	}
+//
+//
+//	public Product getProduct() {
+//		return product;
+//	}
+//
+//
+//	public void setProduct(Product product) {
+//		this.product = product;
+//	}
+//
+//
+//	public String getOfferStatus() {
+//		return offerStatus;
+//	}
+//
+//
+//	public void setOfferStatus(String offerStatus) {
+//		this.offerStatus = offerStatus;
+//	}
+//
+//
+//	public String getRolename() {
+//		return rolename;
+//	}
+//
+//
+//	public void setRolename(String rolename) {
+//		this.rolename = rolename;
+//	}
+//
+//
+//	public Date getStartDate() {
+//		return startDate;
+//	}
+//
+//
+//	public void setStartDate(Date startDate) {
+//		this.startDate = startDate;
+//	}
+//
+//
+//	public Date getEndDate() {
+//		return endDate;
+//	}
+//
+//
+//	public void setEndDate(Date endDate) {
+//		this.endDate = endDate;
+//	}
+//	
+//	
+	
+	
+	
+	
+	
 
 }
--- src/main/java/com/fdmgroup/RentalPlatform/model/Message.java
+++ src/main/java/com/fdmgroup/RentalPlatform/model/Message.java
@@ -1,5 +1,7 @@
 package com.fdmgroup.RentalPlatform.model;
 
+import java.sql.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,5 +11,59 @@ public class Message {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+//	private User sender, recipient;
+//	private String offerStatus, rolename, messageContent;
+//	private Date startDate;
+//	
+//	
+//	
+//	public Message() {
+//		
+//	}
+//	
+//	
+//	
+//	
+//	
+//	public User getSender() {
+//		return sender;
+//	}
+//	public void setSender(User sender) {
+//		this.sender = sender;
+//	}
+//	public User getRecipient() {
+//		return recipient;
+//	}
+//	public void setRecipient(User recipient) {
+//		this.recipient = recipient;
+//	}
+//	public String getOfferStatus() {
+//		return offerStatus;
+//	}
+//	public void setOfferStatus(String offerStatus) {
+//		this.offerStatus = offerStatus;
+//	}
+//	public String getRolename() {
+//		return rolename;
+//	}
+//	public void setRolename(String rolename) {
+//		this.rolename = rolename;
+//	}
+//	public String getMessageContent() {
+//		return messageContent;
+//	}
+//	public void setMessageContent(String messageContent) {
+//		this.messageContent = messageContent;
+//	}
+//	public Date getStartDate() {
+//		return startDate;
+//	}
+//	public void setStartDate(Date startDate) {
+//		this.startDate = startDate;
+//	}
+	
+	
 
+	
 }
