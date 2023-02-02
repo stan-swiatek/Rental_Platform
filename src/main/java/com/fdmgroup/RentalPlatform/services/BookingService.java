804adb26049c7b53c54957a3c3f4c669d844db79
christoph neustifter <christoph.neustifter@fdmgroup.com>
2023-02-02 11:20:28 +0000

    Update BookingService.java

--- src/main/java/com/fdmgroup/RentalPlatform/services/BookingService.java
+++ src/main/java/com/fdmgroup/RentalPlatform/services/BookingService.java
@@ -1,8 +1,13 @@
 package com.fdmgroup.RentalPlatform.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fdmgroup.RentalPlatform.model.Booking;
+import com.fdmgroup.RentalPlatform.model.Product;
+import com.fdmgroup.RentalPlatform.model.User;
 import com.fdmgroup.RentalPlatform.repository.BookingRepository;
 
 
@@ -12,5 +17,25 @@ public class BookingService implements IBookingService {
 	
 	@Autowired
 	private BookingRepository repo;
+
+	@Override
+	public List<Booking> findByUser(User user) {
+		return repo.findByUser(user);
+	}
+
+	@Override
+	public List<Booking> findByProduct(Product product) {
+		return repo.findByProduct(product);
+	}
+
+	@Override
+	public List<Booking> findAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public void saveBooking(Booking booking) {
+		repo.save(booking);		
+	}
 
 }
